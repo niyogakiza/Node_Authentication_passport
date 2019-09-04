b01ce19571db630e2aa4a56615dae81f8ad0c896
Aimable <niyoai@yahoo.fr>
2019-09-04 06:52:41 +0100

    user schema authentication

--- src/users.js
+++ src/users.js
@@ -1,3 +1,25 @@
 const mongoose = require("mongoose");
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  email: String,
+  hash: String,
+  salt: String
+});
+
+UserSchema.methods.setPassword = password => {
+  this.salt = crypto.randomBytes(16).toString("hex");
+  this.hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 512, "sha512")
+    .toString("hex");
+};
+
+UserSchema.methods.validatePassword = password => {
+  const hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 512, "sha512")
+    .toString("hex");
+  return this.hash === hash;
+};
