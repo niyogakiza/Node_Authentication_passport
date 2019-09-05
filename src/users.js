484acb22177188a4d8cd3abe61e64d70f7354a1e
Aimable <niyoai@yahoo.fr>
2019-09-05 06:43:12 +0100

    User authentication

--- src/users.js
+++ src/users.js
@@ -23,3 +23,28 @@ UserSchema.methods.validatePassword = password => {
     .toString("hex");
   return this.hash === hash;
 };
+
+UserSchema.methods.generateJWT = function() {
+  const today = new Date();
+  const expirationDate = new Date(today);
+  expirationDate.setDate(today.getDate() + 60);
+
+  return jwt.sign(
+    {
+      email: this.email,
+      id: this._id,
+      exp: parseInt(expirationDate.getTime() / 1000, 10)
+    },
+    "secret"
+  );
+};
+
+UserSchema.methods.toAuthJson = () => {
+  return {
+    _id: this._id,
+    email: this.email,
+    token: this.generateJWT()
+  };
+};
+
+mongoose.model("Users", UserSchema);
