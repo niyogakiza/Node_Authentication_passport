77d81cd547642c91094db8a87f8e9a88934885d5
Aimable <niyoai@yahoo.fr>
2019-09-10 08:00:08 +0200

    importing dependencies

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
 const passport = require("passport");
 const router = require("express").Router();
-const auth = require("");
+const auth = require("../auth");
+const Users = mongoose.model("Users");
